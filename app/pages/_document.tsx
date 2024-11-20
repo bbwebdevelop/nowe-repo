53b50aabffe682566ab33c7b521ba09a6e6f7249
bbwebdevelop <bbwebdevelop@gmail.com>
2024-11-20 09:35:23 +0100

    Udało się zbudować lokalnie - gotowe do wdrożenia

---
+++ app/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Możesz tu dodać globalne metatagi lub linki do czcionek */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
